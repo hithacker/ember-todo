514ee7295f9311d976d56f4b70ec037da1e26f56
Hiren Thacker <hithacker@gmail.com>
2016-11-07 15:58:46 +0530

    Done till transitioning back to Show all Todos

--- js/router.js
+++ js/router.js
@@ -1,5 +1,9 @@
 Todos.Router.map(function() {
-    this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function () {
+        //additional child routes will go here later
+        this.route('active');
+        this.route('completed');
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
@@ -7,3 +11,64 @@ Todos.TodosRoute = Ember.Route.extend({
     return this.store.find('todo');
   }
 });
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('todos');
+    }
+})
+
+Todos.CommonRouteLogic = Ember.Mixin.create({
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    },
+    /**
+     * You can use this function to get all todos that are either complete or incomplete depending on the parameter that you pass.
+     * @param {boolean} completed - You can choose whether you want to get all complete todos or all incomplete todos.
+     */
+    getAll: function(completed) {
+        return this.store.filter('todo', function(todo) {
+            return completed ?
+                todo.get('isCompleted') :
+                !todo.get('isCompleted');
+        });
+    }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend(Todos.CommonRouteLogic, {
+    model: function() {
+        return this.getAll(false);
+    }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend(Todos.CommonRouteLogic, {
+    model: function() {
+        return this.getAll(true);
+    }
+})
+
+
+//Below is the code that has route duplication
+/*
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.filter('todo', function(todo) {
+            return !todo.get('isCompleted');
+        });
+    }
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.filter('todo', function(todo) {
+            return todo.get('isCompleted');
+        });
+    }
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
+})
+*/
